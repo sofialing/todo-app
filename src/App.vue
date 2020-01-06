3fc0f2f5269ebc8ad60ef75e8ec4cca11d5d788d
Sofia Ling <sofialing@live.se>
2020-01-06 12:29:33 +0100

    Adjusted components

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
-	<div id="app" class="container my-5">
-		<TodayInfo />
-		<TodoList />
-	</div>
+  <div id="app">
+    <TodayInfo />
+    <TodoList />
+  </div>
 </template>
 
 <script>
-import TodayInfo from './components/TodayInfo.vue';
-import TodoList from './components/TodoList.vue';
+import TodayInfo from "./components/TodayInfo.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-	name: 'app',
-	components: {
-		TodayInfo,
-		TodoList
-	}
+  name: "app",
+  components: {
+    TodayInfo,
+    TodoList
+  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,600&display=swap');
-@import 'https://cdn.jsdelivr.net/npm/animate.css@3.5.1';
+@import url("https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,600&display=swap");
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 
 body {
-	background-color: #1e3a40;
-	color: #ffffff;
-	font-family: 'Barlow Semi Condensed', sans-serif;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e3a40;
+  color: #ffffff;
+  font-family: "Barlow Semi Condensed", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.container {
-	max-width: 450px;
+#app {
+  width: 100vw;
+  padding: 1.5rem;
+}
+
+@media screen and (min-width: 450px) {
+  #app {
+    max-width: 414px;
+  }
 }
 </style>
--- src/components/TodayInfo.vue
+++ src/components/TodayInfo.vue
@@ -1,28 +1,28 @@
 <template>
-	<header class="text-center mb-5">
-		<h1 class="display-3 font-weight-normal">{{ time }}</h1>
-		<span class="lead font-weight-light">Today is {{ date }}</span>
-	</header>
+  <header class="text-center mb-4">
+    <h1 class="display-3 font-weight-normal">{{ time }}</h1>
+    <span class="lead font-weight-light">Today is {{ date }}</span>
+  </header>
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 
 export default {
-	name: 'TodayInfo',
-	data() {
-		return {
-			date: moment().format('dddd, MMMM Do YYYY'),
-			time: moment().format('HH:mm')
-		};
-	},
-	created() {
-		setInterval(this.updateClock, 1000);
-	},
-	methods: {
-		updateClock() {
-			this.time = moment().format('HH:mm');
-		}
-	}
+  name: "TodayInfo",
+  data() {
+    return {
+      date: moment().format("dddd, MMMM Do YYYY"),
+      time: moment().format("HH:mm")
+    };
+  },
+  created() {
+    setInterval(this.updateClock, 1000);
+  },
+  methods: {
+    updateClock() {
+      this.time = moment().format("HH:mm");
+    }
+  }
 };
 </script>
